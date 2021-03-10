1c7f97bb51bff1b37b8dc0e648dc4538b9f666f9
liuxiao <18389677071@163.com>
2021-03-11 02:18:13 +0800

    链表(part1): 实现API
    1. 基本实现API
    2. CustomStringConvertible未实现
    3. 未测试功能

--- DataStructure_Swift/DataStructure_Swift/2.链表/LXLXLinkedListTest.swift
+++ DataStructure_Swift/DataStructure_Swift/2.链表/LXLXLinkedListTest.swift
@@ -10,4 +10,17 @@ import Cocoa
 
 class LXLXLinkedListTest: NSObject {
 
+    
+    public func linkedListTest1() {
+       let linkedlist = LXLinkedList<Int>()
+        linkedlist.add(index: 0, element: 1)
+        linkedlist.add(index: 1, element: 22)
+        linkedlist.add(index: 2, element: 333)
+        linkedlist.add(index: 3, element: 4444)
+        linkedlist.add(index: 4, element: 55555)
+        
+        
+    }
+    
+    
 }
--- DataStructure_Swift/DataStructure_Swift/2.链表/LXLinkedList.swift
+++ DataStructure_Swift/DataStructure_Swift/2.链表/LXLinkedList.swift
@@ -8,19 +8,27 @@
 
 import Cocoa
 
+protocol addd {
+    
+}
+
 class LXLinkedList<T>: NSObject, LXCheckRangeable {
     
     //LXCheckRangeable
-    
     var size: Int = 0
+    
+    //CustomStringConvertible
+    override var description: String {
+        "LXLinkedList"
+    }
     
     //节点
     private class ListNode<T> {
         var val: T
         var next: ListNode?
-        init(_ val: T) {
+        init(_ val: T, next: ListNode<T>?) {
             self.val = val
-            self.next = nil
+            self.next = next
         }
     }
     
@@ -32,14 +40,19 @@ class LXLinkedList<T>: NSObject, LXCheckRangeable {
      * @retuen node
      */
     private func node(index: Int) -> ListNode<T> {
-        return first!
+        var node = first
+        for _ in 0..<index {
+            node = node?.next
+        }
+        return node!
     }
     
     /*
      * 清空所有元素
      */
     public func clear() {
-        
+        size = 0
+        first = nil
     }
     
     /*
@@ -48,7 +61,8 @@ class LXLinkedList<T>: NSObject, LXCheckRangeable {
      * @retuen node
      */
     public func get(index:Int) -> T {
-        return "" as! T
+        let indexNode = node(index: index)
+        return indexNode.val
     }
     
     /*
@@ -57,7 +71,9 @@ class LXLinkedList<T>: NSObject, LXCheckRangeable {
      * @retuen node
      */
     public func set(index:Int, element:T) -> T {
-        return "" as! T
+        let indexNode = node(index: index)
+        indexNode.val = element
+        return indexNode.val
     }
     
     /*
@@ -66,7 +82,18 @@ class LXLinkedList<T>: NSObject, LXCheckRangeable {
      * @retuen node
      */
     public func add(index:Int, element:T) {
+        rangeCheckForAdd(index: index)
         
+        if index == 0 {
+            let node = ListNode(element, next: first)
+            first = node
+        }else {
+            let prev =  node(index: index - 1)
+            let node = ListNode(element, next: prev.next)
+            prev.next = node
+        }
+       
+        size += 1
     }
     
     
@@ -74,13 +101,36 @@ class LXLinkedList<T>: NSObject, LXCheckRangeable {
      * 移除index位置对应的元素
      */
     public func remove(index: Int) -> T {
-        return "" as! T
+        rangeCheck(index: index)
+
+        let indexnode = node(index: index)
+        if index == 0  {
+            first = indexnode.next
+        }else {
+            let prev =  node(index: index - 1)
+            prev.next = indexnode.next
+        }
+        size -= 1
+        return indexnode.val
     }
     
     /*
      * 查看元素对应的位置
      */
     public func indexOf(element:T) -> Int {
+        var node = first
+        guard node != nil else {
+            //若无头结点,则返回0
+            return 0
+        }
+        
+        for i in 0 ..< size {
+            if ((node?.val = element) != nil) {
+                return i
+            }
+            node = node?.next
+        }
+        
         return 0
     }
     
